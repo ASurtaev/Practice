963cf97ca9cf9e88882bac76abfa74982b65f627
Alexnader <42913527+HentschelA@users.noreply.github.com>
2019-06-30 21:59:58 +0700

    Add files via upload

---
+++ oppcastle-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="oppcastle-tiles" tilewidth="32" tileheight="32" tilecount="143" columns="13">
+ <image source="oppcastle-tiles.png" width="416" height="352"/>
+</tileset>
